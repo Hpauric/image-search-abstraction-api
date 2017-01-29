551622a822bba9e9c86e75e5b46dd7e1f97dec23
Pauric Holleran <pauric.holleran@gmail.com>
2017-01-29 16:01:53 +0000

    public folder path used

--- app.js
+++ app.js
@@ -70,6 +70,8 @@ function getLastTenSearchTerms(callback) {
   });
 }
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
